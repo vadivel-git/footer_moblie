c5067decf31280c524001bc67fe12257a8e3d791
Design <r.vadivel.vanan@gmail.com>
2017-04-11 00:24:26 -0400

    Add files via upload

---
+++ footer_menu/footer_menu.php
@@ -0,0 +1,98 @@
+<footer id="cv-footer" class="cv-section cv-footer1">
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-10 cv-nopad1 cv-centerpage">
+
+    <div class="cvfo-content">
+      <nav class="cv-directory cv-5clu">
+    <!-- block 1 -->
+    <div class="col-xs-12 col-sm-3">
+        <div class="cv-directory-column">
+          <input class="cv-state" type="checkbox" id="cv-1">
+          <div class="cv-directory-column-section">
+            <label class="cv-directory-column-section-label" for="cv-1">
+              <h3 class="cv-title">About Apple</h3> </label>
+            <a href="#" class="cv-anchor cv-nopen"> <span class="cv-lab">Open Menu</span> </a>
+            <a href="#" class="cv-anchor cv-nclose"> <span class="cv-lab">Close Menu</span> </a>
+            <ul class="cv-directory-column-section-list">
+              <li class="cv-item"><a class="cv-dirl" href="#">Apple Info</a></li>
+              <li class="cv-item"><a class="cv-dirl" href="#">Newsroom</a></li>
+              <li class="cv-item"><a class="cv-dirl" href="#">Job Opportunities</a></li>
+              <li class="cv-item"><a class="cv-dirl" href="#">Press Info</a></li>
+            </ul>
+          </div>
+        </div>
+    </div>
+    <!-- block 1 -->
+
+     <!-- block 1 -->
+    <div class="col-xs-12 col-sm-3">
+        <div class="cv-directory-column">
+          <input class="cv-state" type="checkbox" id="cv-2">
+          <div class="cv-directory-column-section">
+            <label class="cv-directory-column-section-label" for="cv-2">
+              <h3 class="cv-title">About Apple</h3> </label>
+            <a href="#" class="cv-anchor cv-nopen"> <span class="cv-lab">Open Menu</span> </a>
+            <a href="#" class="cv-anchor cv-nclose"> <span class="cv-lab">Close Menu</span> </a>
+            <ul class="cv-directory-column-section-list">
+              <li class="cv-item"><a class="cv-dirl" href="#">Apple Info</a></li>
+              <li class="cv-item"><a class="cv-dirl" href="#">Newsroom</a></li>
+              <li class="cv-item"><a class="cv-dirl" href="#">Job Opportunities</a></li>
+            </ul>
+          </div>
+        </div>
+    </div>
+    <!-- block 1 -->
+
+    <!-- block 1 -->
+    <div class="col-xs-12 col-sm-3">
+        <div class="cv-directory-column">
+          <input class="cv-state" type="checkbox" id="cv-3">
+          <div class="cv-directory-column-section">
+            <label class="cv-directory-column-section-label" for="cv-3">
+              <h3 class="cv-title">About Apple</h3> </label>
+            <a href="#" class="cv-anchor cv-nopen"> <span class="cv-lab">Open Menu</span> </a>
+            <a href="#" class="cv-anchor cv-nclose"> <span class="cv-lab">Close Menu</span> </a>
+            <ul class="cv-directory-column-section-list">
+              <li class="cv-item"><a class="cv-dirl" href="#">Apple Info</a></li>
+              <li class="cv-item"><a class="cv-dirl" href="#">Newsroom</a></li>
+              <li class="cv-item"><a class="cv-dirl" href="#">Job Opportunities</a></li>
+              <li class="cv-item"><a class="cv-dirl" href="#">Press Info</a></li>
+            </ul>
+          </div>
+        </div>
+    </div>
+    <!-- block 1 -->
+
+    <div class="col-xs-12 col-sm-3">
+          <div class="cv-foot-tit">Support</div>
+          <img src="img/paypal.png">
+        </div>
+
+      </nav>
+    </div>
+
+
+    </div>
+
+            <div class="col-xs-12 col-sm-10 cv-nopad1 cv-centerpage">
+        <div class="col-xs-12 col-sm-12 text-center cv-social-a">
+        <div class="cv-social">Follow us on social media</div>
+          <a href="#" target="_blank" class="cv-facebook-icon"><i class="fa fa-facebook"></i></a>
+          <a href="#" target="_blank" class="cv-twitter-icon"><i class="fa fa-twitter"></i></a>
+          <a href="#" target="_blank" class="cv-google-icon"><i class="fa fa-google-plus"></i></a>
+          <a href="#" target="_blank" class="cv-pinterest-icon"><i class="fa fa-pinterest"></i></a>
+        </div>
+        <div class="cv-line"></div>
+        <div class="col-xs-12 col-sm-6 cv-nopadonly">
+          <div class="cv-foot-top">Copyright © 2017 Captioing.com</div>
+        </div>
+        <div class="col-xs-12 col-sm-6 cv-nopadonly text-right">
+          <div class="cv-foot-top1"><a href="#">Help</a></div>
+          <div class="cv-foot-top1"><a href="#">Terms</a></div>
+          <div class="cv-foot-top1"><a href="#">Privacy</a></div>
+        </div>
+        </div>
+  </div>
+</div>
+</footer>
